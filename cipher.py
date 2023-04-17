a4c360795e615e1e88a8bab693e11d25577ead12
pororoarx <kengarrido.legaspi@gmail.com>
2023-04-17 11:24:08 +0800

    Ask user to enter plaintext and keyword

--- cipher.py
+++ cipher.py
@@ -1,6 +1,11 @@
 # Start of pseudocode
 
 # Ask user to enter plaintext message and keyword in all uppercase letters, no spaces. Make the plaintext and keyword in uoppercase.
+plaintext = input("Enter your plaintext (all uppercase letters, no spaces): ")
+plaintext = plaintext.upper()
+
+keyword = input("Enter your keyword (all uppercase letters): ")
+keyword = keyword.upper()
 
 # Create a class called "Vigenere_Cipher" that takes plaintext and keyword as an input
     # Inside the class, initialize the class with the plaintext and keyword
