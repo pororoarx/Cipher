c85da178537caca8f59a0abc6467676ab894597f
pororoarx <kengarrido.legaspi@gmail.com>
2023-04-17 11:20:53 +0800

    Pseudocode

---
+++ cipher.py
@@ -0,0 +1,20 @@
+# Start of pseudocode
+
+# Ask user to enter plaintext message and keyword in all uppercase letters, no spaces. Make the plaintext and keyword in uoppercase.
+
+# Create a class called "Vigenere_Cipher" that takes plaintext and keyword as an input
+    # Inside the class, initialize the class with the plaintext and keyword
+
+    # Create a function called "cipher" to define the cipher method for the Vigenere_Cipher class
+        # Create an empty string "ciphertext"
+        # Set the variable "keyword_letter" to 0
+
+        # Loop through each letter in the plaintext and encrypt each letter with Vigenere_Cipher
+
+
+        # Return the ciphertext string
+
+# Create a new object of the Vigenere_Cipher
+# Execute the cipher method
+
+# Print the cipher text message
