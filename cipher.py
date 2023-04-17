aab2e422495c9e8c6e1709fa1232c8c8650df54b
pororoarx <kengarrido.legaspi@gmail.com>
2023-04-17 11:35:56 +0800

    Created a function; looped through and encryted each letter with Vigenere_Cipher

--- cipher.py
+++ cipher.py
@@ -16,10 +16,19 @@ class Vigenere_Cipher:
         self.key_letter = [ord(letter) - 65 for letter in self.keyword]
 
     # Create a function called "cipher" to define the cipher method for the Vigenere_Cipher class
+    def cipher(self):
         # Create an empty string "ciphertext"
+        ciphertext = ""
         # Set the variable "keyword_letter" to 0
+        keyword_letter = 0
 
         # Loop through each letter in the plaintext and encrypt each letter with Vigenere_Cipher
+        for i in range (len(self.plaintext)):
+            current_plain_letter = self.plaintext[i]
+            key_number = self.key_letter[keyword_letter % len(self.keyword)]
+            cipher_number = (ord(current_plain_letter) - 65 + key_number) % 26
+            ciphertext += chr(cipher_number + 65)
+            keyword_letter = (keyword_letter + 1) % len(self.keyword)
 
 
         # Return the ciphertext string
@@ -27,4 +36,4 @@ class Vigenere_Cipher:
 # Create a new object of the Vigenere_Cipher
 # Execute the cipher method
 
-# Print the cipher text message
+# Print the cipher text message 
